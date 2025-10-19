a917f20a9dc0ae003bf732d670bcf15450918900
mitrabhanu-123 <60176270+mitrabhanu-123@users.noreply.github.com>
2025-10-19 11:45:28 +0530

    Update Main.java
    changes in wrong error

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ class Main{
     public static void main(String[] args){
         System.out.println("Hello world");
         Student st = new Student();
-        st.run()
+        st.run();
     }
 }
